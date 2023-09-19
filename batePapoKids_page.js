cdcdbd3597e6608993ba0ca2f77726efc0ed64e3
Glaucimara Moreira <80214705+glaucimara-jpg@users.noreply.github.com>
2023-09-19 18:13:25 -0300

    Update batePapoKids_page.js

--- batePapoKids_page.js
+++ batePapoKids_page.js
@@ -1,11 +1,11 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyDI5IpFsKVj7Kdj0KMp6Okju1d13tevw_k",
-  authDomain: "conversa-928d8.firebaseapp.com",
-  databaseURL: "https://conversa-928d8-default-rtdb.firebaseio.com",
-  projectId: "conversa-928d8",
-  storageBucket: "conversa-928d8.appspot.com",
-  messagingSenderId: "872961403304",
-  appId: "1:872961403304:web:b51a89d536930e8fe7959d"
+var  firebaseConfig = {
+  apiKey: "AIzaSyB1iZQ-ReaPqyLw_M5I9MgdpHpi_cW97T0",
+  authDomain: "conversa-3b565.firebaseapp.com",
+  databaseURL: "https://conversa-3b565-default-rtdb.firebaseio.com",
+  projectId: "conversa-3b565",
+  storageBucket: "conversa-3b565.appspot.com",
+  messagingSenderId: "256207699608",
+  appId: "1:256207699608:web:a83ce6f665294b4aa81aed"
 };
   
   
